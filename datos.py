97638a117aecc08e269eaf12b83e3239075c9107
Rav1d <erikalvarez93081@gmail.com>
2026-08-27 12:30:10 -0600

    refactor: separa los datos del CRUD

---
+++ datos.py
@@ -0,0 +1,20 @@
+libros = {
+    1: {
+        "titulo": "El principito",
+        "autor": "Antoine Exupery",
+        "isbn": "1234567891234",
+        "editorial": "Pajarito",
+        "paginas": 121,
+        "precio": 873,
+        "disponible": True
+    },
+    2: {
+        "titulo": "El extranjero",
+        "autor": "Albert Camus",
+        "isbn": "1234567891231",
+        "editorial": "Jose Cuervo",
+        "paginas": 435,
+        "precio": 299,
+        "disponible": True
+    }
+}
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import os
 from validaciones import validar_texto, validar_numero, validar_isbn
-
-libros = {
-    1: {
-        "titulo": "El principito",
-        "autor": "Antoine Exupery",
-        "isbn": "1234567891234",
-        "editorial": "Pajarito",
-        "paginas": 121,
-        "precio": 873,
-        "disponible": True
-    },
-    2: {
-        "titulo": "El extranjero",
-        "autor": "Albert Camus",
-        "isbn": "1234567891231",
-        "editorial": "Jose Cuervo",
-        "paginas": 435,
-        "precio": 299,
-        "disponible": True
-    }
-}
+from datos import libros
 
 #! LIMPIAR
 def limpiar_pantalla():
